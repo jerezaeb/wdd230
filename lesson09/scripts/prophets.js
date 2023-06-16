1ce48070890525a8134c4922f57eae1731bc9457
jerezaeb <jerezaespi@gmail.com>
2023-06-16 09:41:14 +0800

    json

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -21,6 +21,12 @@ fetch (url)
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
       let portrait = document.createElement('img');
+      let birthdate = document.createElement ('p');
+      let birthplace = document.createElement ('p');
+      const numofchildren = document.createElement ('p');
+      const length = document.createElement ('p');
+      let death = document.createElement ('p');
+
   
       // Build the h2 content out to show the prophet's full name - finish the template string
       h2.textContent = `${prophet.name} ${prophet.lastname}`;
@@ -31,11 +37,29 @@ fetch (url)
       portrait.setAttribute('loading', 'lazy');
       portrait.setAttribute('width', '340');
       portrait.setAttribute('height', '440');
+
+
+      birthdate.innerHTML= `Date of Birth: ${prophet.birthdate}`;
+      birthplace.innerHTML = `Place of Birth: ${prophet.birthplace}`;
+      numofchildren.innerHTML = `Children: ${prophet.numofchildren}`;
+      length.innerHTML = `Length of Service: ${prophet.length}`;
+      death.innerHTML = `Date of Death: ${prophet.death}`;
+    
+
+
+
   
       // Append the section(card) with the created elements
       card.append(h2);
       card.append(portrait);
       cards.append(card);
+      card.append(birthdate);
+      card.append(birthplace);
+      card.append(numofchildren)
+      card.append(length);
+      card.append(death);
+
+
     }); // end of forEach loop
   // end of function expression
 })
