46d0bcdb3781a302b56495d8d17d212e378ec0b5
jerezaeb <jerezaespi@gmail.com>
2023-06-23 11:04:34 +0800

    weather

--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -3,14 +3,14 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=10.311780&lon=123.916412&q=Fairbanks&units=imperial&appid=4d1acff0243661ed11c7e970e0d0d326';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=4d1acff0243661ed11c7e970e0d0d326';
 async function apiFetch() {
     try {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data); // this is for testing the call
-        // displayResults(data);
+        //console.log(data); // this is for testing the call
+        displayResults(data);
       } else {
           throw Error(await response.text());
       }
@@ -18,5 +18,12 @@ async function apiFetch() {
         console.log(error);
     }
   }
-  
+  function displayResults (weatherData) {
+    currentTemp.textContent = weatherData.main.temp.toFixed(1);
+    const imagesrc = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    weatherIcon.setAttribute('src',imagesrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.innerHTML = desc;
+  }
   apiFetch();
