992aa66636f3b4e6f1ed0832b2326418d1db409e
jerezaeb <jerezaespi@gmail.com>
2023-06-24 23:09:44 +0800

    weather homepage

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -83,6 +83,37 @@ fetch (url)
   // end of function expression
 })
 
+  //weather
   
-  
-  
+  // select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const windSpeed = document.querySelector('#wind-speed');
+
+const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=10.315880299252855&lon=123.88737017449185&units=metric&appid=4d1acff0243661ed11c7e970e0d0d326';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(weatherURL);
+      if (response.ok) {
+        const data = await response.json();
+        //console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  function displayResults (weatherData) {
+    currentTemp.textContent = weatherData.main.temp.toFixed(1);
+    const imagesrc = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    weatherIcon.setAttribute('src',imagesrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.innerHTML = desc;
+    windSpeed.textContent = weatherData.wind.speed.toFixed(2);
+  }
+  apiFetch();
